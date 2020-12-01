7a781bf2c22706b295eccf38baba91d4dbbcfe89
squedd <40431572+squedd@users.noreply.github.com>
2020-12-01 21:47:12 +1000

    removed unused topical folders, add zsh autocd

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,6 +9,7 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
+setopt AUTOCD # automatically cd into dirs without typing 'cd' at the start
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
